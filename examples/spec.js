3ccf8ce2c4cf28f97f5001dc404bc84ebbc05268
Mathias Bynens <mathias@qiwi.be>
2011-01-10 08:52:00 +0100

    Move <script> elements to the bottom, which increases perceived load time if <body> actually contains content. Wrap spec.js in an IIFE to prevent leaking the `spec` variable.

--- examples/spec.js
+++ examples/spec.js
@@ -1,88 +1,92 @@
-// Create a new spec.
-var spec = Spec('Miniatures Unit Tests');
+(function() {
 
-// Bind the event handlers.
-spec.bind('start', function() {
-  // `start` is triggered before any tests are run.
-  console.log('Started spec `%s`.', this.name);
-});
+  // Create a new spec.
+  var spec = Spec('Miniatures Unit Tests');
 
-spec.bind('setup', function(test, spec) {
-  // `setup` is triggered at the start of each test.
-  console.log('Started test `%s`.', test.name);
-});
-
-spec.bind('assertion', function(data, test) {
-  // `assertion` is triggered when an assertion succeeds.
-  console.info('Assertion: %s.', data.message);
-});
-
-spec.bind('failure', function(data) {
-  // `failure` is triggered when an assertion fails.
-  console.error('Failure: %s. Expected: %o. Actual: %o.', data.message,
-    data.expected, data.actual);
-});
-
-spec.bind('error', function(error) {
-  // `error` is triggered when a test throws an error.
-  console.error('Error: %o.', error);
-});
-
-spec.bind('teardown', function(test) {
-  // `teardown` is triggered at the end of each test.
-  console.log('Finished test `%s`. %i assertions, %i failures, %i errors.',
-    test.name, test.assertions, test.failures, test.errors);
-});
-
-spec.bind('complete', function(spec) {
-  // `complete` is triggered once all tests have finished running.
-  console.log('Finished spec `%s`.', spec.name);
-  console.info('%i tests, %i assertions, %i failures, %i errors.', this.length,
-    this.assertions, this.failures, this.errors);
-});
-
-spec.test('ajax', function(test) {
-  Miniatures.ajax({
-    'url': 'spec.html',
-    'complete': function(transport) {
-      test.ok(transport.responseText);
-      test.done(1);
-    }
+  // Bind the event handlers.
+  spec.bind('start', function() {
+    // `start` is triggered before any tests are run.
+    console.log('Started spec `%s`.', this.name);
   });
-});
 
-spec.test('sortBy', function(test) {
-  var data = [{
-    'name': 'John-David Dalton',
-    'nickname': 'jddalton'
-  }, {
-    'name': 'Aaron Beal',
-    'nickname': 'adbeal'
-  }, {
-    'name': 'Kit Goncharov',
-    'nickname': 'ksgoncharov'
-  }, {
-    'name': 'Maddy Jalbert',
-    'nickname': 'mcjalbert'
-  }];
+  spec.bind('setup', function(test, spec) {
+    // `setup` is triggered at the start of each test.
+    console.log('Started test `%s`.', test.name);
+  });
 
-  this.deepEqual(Miniatures.sortBy(data, function(value) {
-    return value.nickname.length;
-  }), [{
-    'name': 'Aaron Beal',
-    'nickname': 'adbeal'
-  }, {
-    'name': 'John-David Dalton',
-    'nickname': 'jddalton'
-  }, {
-    'name': 'Maddy Jalbert',
-    'nickname': 'mcjalbert'
-  }, {
-    'name': 'Kit Goncharov',
-    'nickname': 'ksgoncharov'
-  }]);
+  spec.bind('assertion', function(data, test) {
+    // `assertion` is triggered when an assertion succeeds.
+    console.info('Assertion: %s.', data.message);
+  });
 
-  this.done();
-});
+  spec.bind('failure', function(data) {
+    // `failure` is triggered when an assertion fails.
+    console.error('Failure: %s. Expected: %o. Actual: %o.', data.message,
+      data.expected, data.actual);
+  });
 
-spec.run();
+  spec.bind('error', function(error) {
+    // `error` is triggered when a test throws an error.
+    console.error('Error: %o.', error);
+  });
+
+  spec.bind('teardown', function(test) {
+    // `teardown` is triggered at the end of each test.
+    console.log('Finished test `%s`. %i assertions, %i failures, %i errors.',
+      test.name, test.assertions, test.failures, test.errors);
+  });
+
+  spec.bind('complete', function(spec) {
+    // `complete` is triggered once all tests have finished running.
+    console.log('Finished spec `%s`.', spec.name);
+    console.info('%i tests, %i assertions, %i failures, %i errors.', this.length,
+      this.assertions, this.failures, this.errors);
+  });
+
+  spec.test('ajax', function(test) {
+    Miniatures.ajax({
+      'url': 'spec.html',
+      'complete': function(transport) {
+        test.ok(transport.responseText);
+        test.done(1);
+      }
+    });
+  });
+
+  spec.test('sortBy', function(test) {
+    var data = [{
+      'name': 'John-David Dalton',
+      'nickname': 'jddalton'
+    }, {
+      'name': 'Aaron Beal',
+      'nickname': 'adbeal'
+    }, {
+      'name': 'Kit Goncharov',
+      'nickname': 'ksgoncharov'
+    }, {
+      'name': 'Maddy Jalbert',
+      'nickname': 'mcjalbert'
+    }];
+
+    this.deepEqual(Miniatures.sortBy(data, function(value) {
+      return value.nickname.length;
+    }), [{
+      'name': 'Aaron Beal',
+      'nickname': 'adbeal'
+    }, {
+      'name': 'John-David Dalton',
+      'nickname': 'jddalton'
+    }, {
+      'name': 'Maddy Jalbert',
+      'nickname': 'mcjalbert'
+    }, {
+      'name': 'Kit Goncharov',
+      'nickname': 'ksgoncharov'
+    }]);
+
+    this.done();
+  });
+
+  spec.run();
+
+}());
