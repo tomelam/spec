62d5b6aaa296f0932b24c8c014481a4e7a7e1a89
Kit Goncharov <ksgoncharov@gmail.com>
2011-02-12 09:55:19 -0700

    Fix a typo in the example spec.

--- examples/spec.js
+++ examples/spec.js
@@ -147,7 +147,7 @@
   spec.bind('complete', function(event) {
     // `complete` is triggered once all tests have finished running.
     console.log('Finished spec `' + event.target.name + '`.');
-    console.log(this.length + ' specs, ' + this.assertions + ' assertions, ' + this.failures + ' failures.');
+    console.log(this.length + ' tests, ' + this.assertions + ' assertions, ' + this.failures + ' failures.');
   });
 
   spec.test('ajax', function(test) {
