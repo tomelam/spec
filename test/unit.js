aa3637c85fe7c998ef80ce5dbac8ff602f5b47d7
Kit Cambridge <kitcambridge@me.com>
2012-03-01 00:29:04 -0700

    Remove `Events#removeAllListeners`.

--- test/unit.js
+++ test/unit.js
@@ -440,7 +440,7 @@
     test.equal(events.size, 3, "The `size` property should be incremented each time an event is fired");
 
     events.size = 0;
-    events.removeAllListeners("all").on("all", function () {
+    events.removeListener("all").on("all", function () {
       events.size += 1;
     }).emit("all");
     test.equal(events.size, 1, "The `all` event should not be fired twice if it is explicitly fired");
@@ -448,12 +448,12 @@
     test.done(5);
   });
 
-  testSuite.addTest("Events#removeAllListeners", function () {
+  testSuite.addTest("Events#removeListener", function () {
     var events = new Spec.Events();
     events.size = 0;
     events.on("test", function () {
       events.size += 1;
-    }).emit("test").removeAllListeners("test").emit("test");
+    }).emit("test").removeListener("test").emit("test");
     this.equal(events.size, 1, "The `size` property should have only been incremented once, after which the event handler is removed");
     this.ok(Newton.isEmpty(events.events), "The event registry should be empty");
     this.done(2);
@@ -541,7 +541,7 @@
       events.on("test all", callback);
     }).emit("test");
     this.notOk(size, "`on` invocations should not affect the behavior of `emit`");
-    events.removeAllListeners().on("test", function () {
+    events.removeListener().on("test", function () {
       events.removeListener("test all", callback);
     }).on("test all", callback).emit("test");
     this.equal(size, 2, "`removeListener` invocations should not affect the behavior of `emit`");
